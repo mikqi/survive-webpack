d594de96a9a946b288d593dfe5bcf86a13396a99
Muhammad Rivki <muhammad.rivki@outlook.com>
2016-10-30 17:25:35 +0700

    Analyzing Build Statistics
    Tutorial detail can read in this link: http://survivejs.com/webpack/building-with-webpack/analyzing-build-statistics/

--- webpack.config.js
+++ webpack.config.js
@@ -85,6 +85,32 @@ switch (process.env.npm_lifecycle_event) {
       parts.purifyCSS([PATHS.app])
     );
     break;
+  case 'stats':
+    config = merge(
+      common,
+      {
+        devtool: 'source-map',
+        output: {
+          path: PATHS.build,
+          filename: '[name].[chunkhash].js',
+          chunkFilename: '[chunkhash].js',
+        },
+      },
+      parts.clean(PATHS.build),
+      parts.setFreeVariable(
+        'process.env.NODE_ENV',
+        'production'
+      ),
+      parts.extractBundle({
+        name: 'vendor',
+        entries: ['react'],
+      }),
+      parts.minify(),
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.app])
+    );
+
+    break;
   default:
     config = merge(
       common,
@@ -100,4 +126,6 @@ switch (process.env.npm_lifecycle_event) {
       }));
 }
 
-module.exports = validate(config);
+module.exports = validate(config, {
+  quiet: true,
+});

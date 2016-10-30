ca581aa616acb01702940b972b1b9c00087c939b
Muhammad Rivki <muhammad.rivki@outlook.com>
2016-10-30 11:50:17 +0700

    Enabling Sourcemaps
    tutorial can read in this link: http://survivejs.com/webpack/developing-with-webpack/enabling-sourcemaps/

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,9 @@ const common = {
    */
   output: {
     path: PATHS.build,
-    filename: '[name].js',
+    filename: '[name].[hash].js',
+    sourceMapFilename: '[file].map',
+    devtoolModuleFilenameTemplate: 'webpack',
   },
 
   /**
@@ -56,11 +58,17 @@ switch (process.env.npm_lifecycle_event) {
   case 'build':
     config = merge(
       common,
+      {
+        devtool: 'source-map',
+      },
       parts.setupCSS(PATHS.app));
     break;
   default:
     config = merge(
       common,
+      {
+        devtool: 'eval-source-map',
+      },
       parts.setupCSS(PATHS.app),
       parts.devServer({
         // untuk mengkustom host/port jika dibutuhkan
